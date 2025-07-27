2483c2b32304192987718509191a9bc6e4374057
Davit Petrosyan <davit@skyntco.com>
2025-07-27 16:35:57 -0400

    Allow trialing users in checkSubscriptionStatus

---
+++ src/app/api/subscription-status/route.ts
@@ -0,0 +1,14 @@
+// app/api/subscription-status/route.ts
+import { auth } from "@clerk/nextjs";
+import { checkSubscriptionStatus } from "@/lib/subscription";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { userId } = auth();
+
+  if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+  const isActive = await checkSubscriptionStatus(userId);
+
+  return NextResponse.json({ isActive });
+}
