7737800034060ee6fc64356d0da6cc463c318c18
Davit Petrosyan <davit@skyntco.com>
2025-08-05 09:20:38 -0400

    🐛 Fix PostCSS config for Vercel: switch to CommonJS format

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-export default {
-  plugins: [tailwindcss, autoprefixer],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
