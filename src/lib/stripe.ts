23df4b51b4f07c69ab42867bc5b80d923e70b4a2
Davit Petrosyan <davit@skyntco.com>
2025-08-24 11:49:44 -0400

    Fix: Stripe Version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // Optional: keep consistent with your Stripe dashboard default
-  apiVersion: "2025-06-30.basil",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+// no apiVersion override
 
 export default stripe;
